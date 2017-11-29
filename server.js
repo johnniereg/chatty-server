665df9d40df90197310d18da2ee532c951279830
Johnnie Regalado <johnniereg@gmail.com>
2017-11-29 18:45:25 +0000

    Tracking client connections.
    Counting total connections and logging messages on open and closed connections.

--- server.js
+++ server.js
@@ -15,12 +15,16 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
+let totalClients = 0;
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
     console.log('Client connected');
+    totalClients += 1;
 
     // Set up a callback for when a client closes the socket. This usually means they closed their browser.
     ws.on('close', () => console.log('Client disconnected'));
+    totalClients -= 1;
 });
