b6ad0c402ab4e0e0d5871506a20d1792f2b78634
Johnnie Regalado <johnniereg@gmail.com>
2017-11-29 21:11:42 +0000

    Server receives messages, then sends to active.
    If socket is active, it receives the pushed message.

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 // server.js
 
+const ws = require('ws');
 const express = require('express');
-const SocketServer = require('ws').Server;
+// const SocketServer = require('ws').Server;
 const PORT = 3001;
+const uuidv1 = require('uuid/v1');
 
 const server = express()
     .use(express.static('public'))
     .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${PORT}`));
 
 // Create the WebSockets server
-const chat = new SocketServer({ server });
+const chat = new ws.Server({ server });
 
 // Array for tracking client.
 const clients = [];
@@ -19,7 +21,7 @@ let totalClients = 0;
 
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
+// the socket parameter in the callback.
 chat.on('connection', (socket) => {
     clients.push(socket);
     console.log('Client connected');
@@ -28,11 +30,24 @@ chat.on('connection', (socket) => {
     socket.on('message', function incoming(message) {
         let messageObj = JSON.parse(message);
         console.log('User: ' + messageObj.username + ' said: ' + messageObj.content);
+
+        let broadcastMsg = {
+            id: uuidv1(),
+            username: messageObj.username,
+            content: messageObj.content
+        }
+
+        clients.forEach((client) => {
+            if (client.readyState === ws.OPEN) {
+                client.send(JSON.stringify(broadcastMsg));
+            }
+        });
+        
     });
 
     // Set up a callback for when a client closes the socket. This usually means they closed their browser.
     socket.on('close', () => {
-        console.log('Client disconnected')
+        console.log('Client disconnected');
         totalClients -= 1;
     });
 });
