cc75ee0b5ad9dfa9171eae58dbb947b8e7e8f06a
Johnnie Regalado <johnniereg@gmail.com>
2017-11-29 19:55:17 +0000

    Receiving JSON obj with messages.
    Server receives client messages as JSON object with ID, Username, Content and logs out the message to confirm receipt at server side.

--- server.js
+++ server.js
@@ -2,29 +2,37 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
-
-// Set the port to 3001
 const PORT = 3001;
 
-// Create a new express server
 const server = express()
-    // Make the express server serve static assets (html, javascript, css) from the /public folder
     .use(express.static('public'))
     .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${PORT}`));
 
 // Create the WebSockets server
-const wss = new SocketServer({ server });
+const chat = new SocketServer({ server });
 
+// Array for tracking client.
+const clients = [];
+
+// Counter for tracking active connections.
 let totalClients = 0;
 
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
-wss.on('connection', (ws) => {
+chat.on('connection', (socket) => {
+    clients.push(socket);
     console.log('Client connected');
     totalClients += 1;
 
+    socket.on('message', function incoming(message) {
+        let messageObj = JSON.parse(message);
+        console.log('User: ' + messageObj.username + ' said: ' + messageObj.content);
+    });
+
     // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-    ws.on('close', () => console.log('Client disconnected'));
-    totalClients -= 1;
+    socket.on('close', () => {
+        console.log('Client disconnected')
+        totalClients -= 1;
+    });
 });
